916bfc7c970441d24d66c327df71b786b517bfcc
SEYİT ÇINAR <70111403+cinarseyit@users.noreply.github.com>
2022-12-13 00:28:38 +0300

    Create boot.py

---
+++ kod/boot.py
@@ -0,0 +1,38 @@
+import gc
+import machine
+import network
+import upip
+import senko
+import time
+
+
+def connect_wlan(ssid, password):
+    sta_if = network.WLAN(network.STA_IF)
+    ap_if = network.WLAN(network.AP_IF)
+    sta_if.active(True)
+    ap_if.active(False)
+
+    if not sta_if.isconnected():
+        print("Connecting to WLAN ({})...".format(ssid))
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+
+    return True
+    
+
+SSID = "Test"
+PASSWORD = "lifeline"
+
+connect_wlan(SSID, PASSWORD)
+    
+OTA = senko.Senko(user="cinarseyit", repo="Lifeline", branch = "main", working_dir="code", files=["main.py"])
+
+if OTA.update():
+    print("Updated to the latest version! Rebooting...")
+    time.sleep(5)
+    machine.reset()
+
+
+
